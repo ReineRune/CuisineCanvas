10cdb575c3a9a0d2981e0f3f25e87cfe85640821
ReineRune <reine.sterby@gmail.com>
2023-11-27 12:08:25 +0100

    Project started!

---
+++ cuisineCanvas/src/main/java/group4/cuisineCanvas/CuisineCanvasApplication.java
@@ -0,0 +1,13 @@
+package group4.cuisineCanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuisineCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuisineCanvasApplication.class, args);
+	}
+
+}
---
+++ cuisineCanvas/src/test/java/group4/cuisineCanvas/CuisineCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.cuisineCanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuisineCanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
