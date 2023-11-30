947ce5315ddca60c89bb53f634afa0d2839b015b
ReineRune <reine.sterby@gmail.com>
2023-11-30 11:29:24 +0100

    Created classes singInDto, AuthenticationFilter, AuthenicationService, JwtService to create an EndPoint for User login and Tokengeneration

--- cuisineCanvas/src/main/java/group4/cuisineCanvas/config/SecurityConfig.java
+++ cuisineCanvas/src/main/java/group4/cuisineCanvas/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package group4.cuisineCanvas.config;
 
+import group4.cuisineCanvas.filter.AuthenticationFilter;
+import group4.cuisineCanvas.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,24 +15,54 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+
 public class SecurityConfig {
+
+    private final UserService userService;
+
+    public SecurityConfig(UserService userService) {
+        this.userService = userService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder();
-
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         security
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/signup", "/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .authenticationProvider(authProvider())
+                .addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return security.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
---
+++ cuisineCanvas/src/main/java/group4/cuisineCanvas/dto/SignInDto.java
@@ -0,0 +1,10 @@
+package group4.cuisineCanvas.dto;
+
+import lombok.Data;
+
+@Data
+public class SignInDto {
+
+    private String email;
+    private String password;
+}
